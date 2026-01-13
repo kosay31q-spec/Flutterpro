48536ad76545e3e0efcc1b3ee20bf608c69bf1eb
kosay31q-spec <kosay31q@gmail.com>
2026-01-13 08:20:55 -0800

    Update home.dart

--- screan/home.dart
+++ screan/home.dart
@@ -1,59 +1,207 @@
 import 'package:flutter/material.dart';
 
-class hm extends StatefulWidget {
-  const hm({super.key});
+class Hm extends StatefulWidget {
+  const Hm({super.key});
 
   @override
-  State<hm> createState() => _hmState();
+  State<Hm> createState() => _HmState();
 }
 
-class _hmState extends State<hm> {
+class _HmState extends State<Hm> {
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
-    return Scaffold(appBar: AppBar(
-      leadingWidth: width,
-      backgroundColor: Colors.blue,
-      title: Center(
-        child: Text("الصفحة الرئيسية ", style: TextStyle(color: Colors.white)),
+
+    // أبعاد الأزرار
+    final buttonWidth = width * 0.4;
+    final buttonHeight = height * 0.3;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text(
+          "الصفحة الرئيسية",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (String value) {
+              // التعامل مع الاختيار هنا
+            },
+            itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+              const PopupMenuItem<String>(
+                value: '/we',
+                child: Text('من نحن'),
+              ),
+              const PopupMenuItem<String>(
+                value: '/call',
+                child: Text('اتصل بنا'),
+              ),
+              const PopupMenuItem<String>(
+                value: '/week',
+                child: Text('برنامج الأسبوع'),
+              ),
+              const PopupMenuItem<String>(
+                value: '/logout',
+                child: Text('تسجيل خروج'),
+              ),
+            ],
+          ),
+        ],
+        automaticallyImplyLeading: false,
       ),
-      actions: [
-        Row(
+      body: Container(
+        width: width,
+        height: height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            PopupMenuButton<String>(
-              onSelected: (String value) {},
-              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-                const PopupMenuItem<String>(
-                  value: '/we',
-                  child: ListTile(trailing: Text('من نحن')),itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-                const PopupMenuItem<String>(
-                  value: '/call',
-                  child: ListTile(trailing: Text('اتصل بنا')),itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-                const PopupMenuItem<String>(
-                  value: '/week',
-                  child: ListTile(trailing: Text('برنامج الاسبوع')),itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-                const PopupMenuItem<String>(
-                  value: '/logout',
-                  child: ListTile(trailing: Text('تسجيل خروج ')),
+            // الصف الأول (زرين)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // الزر الأول
+                Container(
+                  width: buttonWidth,
+                  height: buttonHeight,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // إضافة وظيفة الزر الأول
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue[300],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.school, size: 50, color: Colors.white),
+                        SizedBox(height: 10),
+                        Text(
+                          'العلامات ',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+
+                Container(
+                  width: buttonWidth,
+                  height: buttonHeight,
+                  child: ElevatedButton(
+                    onPressed: () {
+
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue[300],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.person, size: 50, color: Colors.white),
+                        SizedBox(height: 10),
+                        Text(
+                          'الغيابات',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 20),
+
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+
+                Container(
+                  width: buttonWidth,
+                  height: buttonHeight,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // إضافة وظيفة الزر الثالث
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue[300],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.home_work, size: 50, color: Colors.white),
+                        SizedBox(height: 10),
+                        Text(
+                          'الواجبات المنزلية',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                // الزر الرابع
+                Container(
+                  width: buttonWidth,
+                  height: buttonHeight,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // إضافة وظيفة الزر الرابع
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue[300],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.note, size: 50, color: Colors.white),
+                        SizedBox(height: 10),
+                        Text(
+                          'ملاحظات',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
           ],
         ),
-      ],
-        automaticallyImplyLeading: false
-    ),
-                body:home:Container(
-  width: width,
-  height:height ,
-  child: Column(
-  children: [
-    // عناصر هنا
-  ],
-)
-)
-                );
+      ),
+    );
   }
 }
-
